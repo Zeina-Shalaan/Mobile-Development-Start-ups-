972e8058960d7d062f184f268881f34477c8d486
Zeina-Shalaan <156028547+Zeina-Shalaan@users.noreply.github.com>
2025-10-16 15:51:50 +0300

    Delete main directory

--- main/java/com/example/MVVM1/view/allproducts/onFavClicked.java
+++
@@ -1,7 +0,0 @@
-package com.example.MVVM1.view.allproducts;
-
-import com.example.MVVM1.model.Product;
-
-public interface onFavClicked {
-    void addToFav(Product product);
-}
--- main/java/com/example/MVVM1/view/favorites/onDeleteClicked.java
+++
@@ -1,7 +0,0 @@
-package com.example.MVVM1.view.favorites;
-
-import com.example.MVVM1.model.Product;
-
-public interface onDeleteClicked {
-    void deleteProduct(Product product);
-}
